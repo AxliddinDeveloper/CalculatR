dd06954e793f51562ed0af61639f1cbcf3314781
Axliddin Xushboqov <axliddinxushboqov7@gmail.com>
2023-06-20 19:04:04 +0500

    INFA: repairing Project

--- Program.cs
+++ Program.cs
@@ -13,8 +13,19 @@ System.Console.Write("birinchi sonni kiriting: ");
 birinchiSon = Convert.ToDecimal(Console.ReadLine());
 System.Console.Write("ikkinchi sonni kiriting: ");
 ikkinchiSon = Convert.ToDecimal(Console.ReadLine());
-System.Console.WriteLine($"qoshish:{birinchiSon + ikkinchiSon}");
-System.Console.WriteLine($"Ayirish:{birinchiSon - ikkinchiSon}");
-System.Console.Write($"bo\'lish:{birinchiSon / ikkinchiSon}");
+System.Console.WriteLine($"birinchi sonni ikkinchi songa qoshsa:{birinchiSon + ikkinchiSon}");
+System.Console.WriteLine($"birinchi sondan ikkinchi sonni Ayirsa:{birinchiSon - ikkinchiSon}");
+System.Console.Write($"birinchi sonni ikkinchi songa bo\'lsa:{birinchiSon / ikkinchiSon}");
 System.Console.WriteLine($" |qoldiq:{birinchiSon % ikkinchiSon}");
-System.Console.WriteLine($"kopaytirish:{birinchiSon * ikkinchiSon}");
+System.Console.WriteLine($"birinchi sonni ikkinchi songa kopaytirsa:{birinchiSon * ikkinchiSon}");
+if (birinchiSon > ikkinchiSon)
+{
+    System.Console.WriteLine("birinchi son ikkinchi sondan katta!");
+}else if (birinchiSon == ikkinchiSon)
+{
+    System.Console.WriteLine("birinchi son ikkinchi songa teng!");
+}
+else
+{
+    System.Console.WriteLine("birinchi son ikkinchi sondan kichik!");
+}
